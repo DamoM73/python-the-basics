c407e86c3c8bb464e16dac8646fdbce6f28f927b
Damien <damomurtagh@gmail.com>
2019-04-29 23:01:40 +1000

    recording

--- tk_02_label.py
+++ tk_02_label.py
@@ -1,17 +1,17 @@
 '''example program for the label widget'''
-#from tkinter import *
+from tkinter import *
 
 
 # **** create window ****
-#root = Tk()
-#root.geometry("300x200")
-#root.title("Tkinter label example")
+root = Tk()
+root.geometry("300x200")
+root.title("Tkinter label example")
 
 
 # **** Add window content ****
-#example_label = Label(root, text="This is the example label")
-#example_label.pack()
+example_label = Label(root, text="This is the example label")
+example_label.pack()
 
 
 # **** Run window loop ****
-#root.mainloop()
+root.mainloop()
